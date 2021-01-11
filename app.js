c447ccf33697fe9c819ed514ad3826c5dc5c1850
Blake <austin.hall8@gmail.com>
2021-01-11 01:54:32 -0500

    added animation added drop down menu

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-"use strict"
+let frontEnd = document.getElementById('welcome');
+quote.style.visibility = 'hidden';
+let image = document.getElementById('img')
+let arr = [];
+
+let menu = document.getElementById('btn');
+let logo = document.getElementById('art');
+
+function logoMotion(evt){
+    evt.target.classList.add('slide 3s');
+}
+
+menu.addEventListener('click', logoMotion);
+
+
+
+//frontEnd.addEventListener('mouseover', changeSize);
+
+
+
+
+
+    
