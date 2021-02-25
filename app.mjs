58e6e8056c99d7b96e18a538a1a0eb22e54b571f
Blake <austin.hall8@gmail.com>
2021-02-24 23:23:39 -0500

    added animation to Skills

---
+++ app.mjs
@@ -0,0 +1,54 @@
+"use strict";
+import { default as anime } from "./node_modules/animejs/lib/anime.es.js";
+import { isInViewport } from "./helper.js";
+/*eslint-env browser*/
+
+let menu = document.getElementById("btn");
+let logo = document.getElementById("art");
+let drop = document.getElementById("content");
+var isAnimating = false;
+
+//added listener to menu to display on click 
+// if displayed click will hide.
+//css animations included
+
+menu.addEventListener("click", function () {
+	if (isAnimating) {
+		drop.style.display = "none";
+		logo.classList.remove("animate");
+		logo.classList.add("pulse");
+		menu.innerHTML = "Menu ▼";
+	} else {
+		drop.style.display = "block";
+		logo.classList.remove("pulse");
+		logo.classList.add("animate");
+		menu.innerHTML = "Welcome";
+	}
+
+	isAnimating = !isAnimating;
+});
+
+// const box = document.querySelector('.box');
+// const message = document.querySelector('#message');
+
+
+const list = Array.from(document.getElementsByClassName('skill'));
+// const listTextContent = listUl.map((elm) => {
+// 	return elm.textContent;
+// });
+// const listText = listTextContent.slice(0, 10);
+
+let timeLine = () => {
+	anime({
+		targets: list,
+		translateY: [-40, 0],
+		opacity: [0.0, 1],
+		delay: anime.stagger(250),
+		duration: 3000,
+		endDelay: 3000,
+		loop: true
+	});
+}
+window.addEventListener('load', () => {
+	timeLine();
+});
---
+++ helper.js
@@ -0,0 +1,11 @@
+"use strict";
+
+export const isInViewport = function (elem) {
+    var bounding = elem.getBoundingClientRect();
+    return (
+        bounding.top >= 0 &&
+        bounding.left >= 0 &&
+        bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+};
